8e1338ace5e043a132daf3937d417ff75a16bf81
quyendv <daovanquyen45@gmail.com>
2024-02-25 16:31:18 +0700

    feat: dynamic user-menu dropdown by role

--- src/components/layouts/header/UserMenu.tsx
+++ src/components/layouts/header/UserMenu.tsx
@@ -14,22 +14,38 @@ import { UserInfo } from '~/contexts/auth.context';
 import { useTranslations } from 'next-intl';
 import { routeConfig } from '~/configs/route.config';
 import { PersonIcon } from '@radix-ui/react-icons';
-import { Luggage, Heart } from 'lucide-react';
+import { Luggage, Heart, LogOutIcon, LayoutDashboardIcon, UsersIcon, HotelIcon } from 'lucide-react';
 import { ILink } from '~/locales/i18nNavigation';
+import { UserRole } from '~/configs/role.config';
 
 interface UserMenuProps {
   user: UserInfo;
   signOut: () => Promise<void>;
 }
 
-const menuItems = [
+const customerMenuItems = [
   { label: 'manageAccount', href: routeConfig.MANAGE_ACCOUNT, icon: PersonIcon },
   { label: 'myBookings', href: routeConfig.MY_BOOKING, icon: Luggage },
   { label: 'saved', href: routeConfig.SAVED, icon: Heart },
 ];
 
+const adminMenuItems = [
+  { label: 'dashboard', href: routeConfig.DASHBOARD, icon: LayoutDashboardIcon },
+  { label: 'manageCustomers', href: routeConfig.MANAGE_CUSTOMERS, icon: UsersIcon },
+  { label: 'manageHotels', href: routeConfig.MANAGE_HOTELS, icon: HotelIcon },
+  // { label: 'manageRooms', href: routeConfig.MANAGE_ROOMS, icon: Luggage },
+];
+
+const hotelMenuItems = [{ label: 'myHotel', href: routeConfig.MY_HOTEL, icon: HotelIcon }];
+
 export function UserMenu({ user, signOut }: UserMenuProps) {
   const t = useTranslations('Header');
+  const menuItems =
+    user.role === UserRole.CUSTOMER
+      ? customerMenuItems
+      : user.role === UserRole.ADMIN
+        ? adminMenuItems
+        : hotelMenuItems;
 
   return (
     <DropdownMenu>
@@ -55,7 +71,7 @@ export function UserMenu({ user, signOut }: UserMenuProps) {
         ))}
         <DropdownMenuSeparator />
         <DropdownMenuItem onClick={signOut}>
-          <PersonIcon className="size-5" />
+          <LogOutIcon className="size-5" />
           <span className="ml-4">{t('signOut')}</span>
         </DropdownMenuItem>
       </DropdownMenuContent>
