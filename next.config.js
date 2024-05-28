5a2d2a76d9cd714943f1e8938739fff82fa358e1
quyendv <daovanquyen45@gmail.com>
2024-05-29 02:32:17 +0700

    feat: update docker image

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const withNextIntl = require('next-intl/plugin')('./src/locales/i18n.ts');
 
 const nextConfig = withNextIntl({
   distDir: '.next', // default
+  output: 'standalone', // https://github.com/vercel/next.js/tree/canary/examples/with-docker
   images: {
     // domains: ['lh3.googleusercontent.com', 'firebasestorage.googleapis.com', 'cf.bstatic.com'],
     remotePatterns: [
